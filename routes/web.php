f86ca4c20f2bcab5a852e23176542c9b620bb961
vassilidev <vassilidevnet@gmail.com>
2024-03-15 21:32:06 +0100

    fix: img

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,24 @@
 Route::get('test', function () {
     $url = "https://manu.qwile.com/wp-content/uploads/sites/7/2024/03/Asset-2@4x-8.png";
 
-    ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
+    function get_contents($url, $ua = 'Mozilla/5.0 (Windows NT 5.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1', $referer = 'http://www.google.com/') {
+        if (function_exists('curl_exec')) {
+            $header[0] = "Accept-Language: en-us,en;q=0.5";
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_USERAGENT, $ua);
+            curl_setopt($curl, CURLOPT_REFERER, $referer);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+            $content = curl_exec($curl);
+            curl_close($curl);
+        }
+        else {
+            $content = file_get_contents($url);
+        }
+        return $content;
+    }
 
-    dump(file_get_contents($url));
-
-    $ch = curl_init();
-
-    // set url
-    curl_setopt($ch, CURLOPT_URL, $url);
-
-    //return the transfer as a string
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-
-    // $output contains the output string
-    $output = curl_exec($ch);
-
-    // close curl resource to free up system resources
-    curl_close($ch);
-
-    dd($output);
+    dump(get_contents($url));
 });
