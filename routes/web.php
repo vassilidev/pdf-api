2a87c383a4cd5fb024984d93e8fc666f29485e77
vassilidev <vassilidevnet@gmail.com>
2024-03-15 21:25:01 +0100

    fix: img

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,20 @@
 Route::get('test', function () {
     $url = "https://manu.qwile.com/wp-content/uploads/sites/7/2024/03/Asset-2@4x-8.png";
 
-    $options = [
-        'ssl' => [
-            'verify_peer'      => false,
-            'verify_peer_name' => false,
-        ],
-    ];
+    $ch = curl_init();
 
-    $context = stream_context_create($options);
+    // set url
+    curl_setopt($ch, CURLOPT_URL, $url);
 
-    dump(file_get_contents($url, false, $context));
+    //return the transfer as a string
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+
+    // $output contains the output string
+    $output = curl_exec($ch);
+
+    // close curl resource to free up system resources
+    curl_close($ch);
+
+    dd($output);
 });
