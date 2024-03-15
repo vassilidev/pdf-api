ba493381f2351c64aca530b3d2658f5cfb890c12
vassilidev <vassilidevnet@gmail.com>
2024-03-15 21:23:08 +0100

    fix: img

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,16 @@
 <?php
+
+Route::get('test', function () {
+    $url = "https://manu.qwile.com/wp-content/uploads/sites/7/2024/03/Asset-2@4x-8.png";
+
+    $options = [
+        'ssl' => [
+            'verify_peer'      => false,
+            'verify_peer_name' => false,
+        ],
+    ];
+
+    $context = stream_context_create($options);
+
+    dump(file_get_contents($url, false, $context));
+});
