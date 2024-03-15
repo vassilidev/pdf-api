9884722add8225f3f3d5635fd0b1c68c92ec77da
vassilidev <vassilidevnet@gmail.com>
2024-03-15 10:29:30 +0100

    fix: image pdf

--- app/Http/Controllers/StreamContractPDFController.php
+++ app/Http/Controllers/StreamContractPDFController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Contract;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class StreamContractPDFController extends Controller
@@ -16,7 +18,7 @@ class StreamContractPDFController extends Controller
     {
         $html = $contract->content;
 
-        if($contract->data) {
+        if ($contract->data) {
             $html = Str::replace(
                 search: array_keys($contract->data),
                 replace: array_values($contract->data),
@@ -24,7 +26,37 @@ class StreamContractPDFController extends Controller
             );
         }
 
-        return Pdf::loadView('pdf', compact('html'))
+        $html = $this->embedImages($html);
+
+        return Pdf::loadHTML($html)
             ->stream($contract->name . '.pdf');
+    }
+
+    /**
+     * Embed images in HTML content as base64-encoded data.
+     */
+    private function embedImages($html)
+    {
+        $pattern = '/<img[^>]+src="([^"]+)"[^>]*>/i';
+
+        $callback = static function ($matches) {
+            try {
+                $imageUrl = $matches[1];
+
+                $imageContent = Http::get($imageUrl)->body();
+
+                $base64 = base64_encode($imageContent);
+
+                $dataUri = 'data:image/' . pathinfo($imageUrl, PATHINFO_EXTENSION) . ';base64,' . $base64;
+
+                return '<img src="' . $dataUri . '">';
+            } catch (\Exception $exception) {
+                Log::error($exception->getMessage());
+            }
+
+            return '';
+        };
+
+        return preg_replace_callback($pattern, $callback, $html);
     }
 }
